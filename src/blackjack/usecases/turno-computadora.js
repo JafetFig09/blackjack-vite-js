0cba905bbfd17dd31304a233607c732e937f8535
jafet <jafetfigueroa09@gmail.com>
2023-08-30 11:11:44 -0600

    first commint

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,46 @@
+import { pedirCarta,valorCarta, crearCartaHTML } from "./index";
+
+/**
+ * 
+ * @param {Number} puntosMinimos 
+ * @param {HTMLElement} puntosHTML
+ * @param {HTMLElement} divCartasComputadora
+ * @param {Array<String>} deck 
+ */
+
+
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = []) => {
+
+    if (!puntosMinimos) throw new Error ("Puntos mínimos son necesarios");
+    if (!puntosHTML) throw new Error ("Puntos HTML es necesario");
+    
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        //TODO: crear carta
+        const imgCarta = crearCartaHTML( carta);
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
